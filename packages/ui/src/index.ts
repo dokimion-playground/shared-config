1de82a574da7f395485ce19abce5572bcd4a3edb
ss-davidlee <davidlee@styleseller.co.kr>
2024-07-24 18:47:35 +0900

    feat: export 추가

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,4 @@
-export { default as DesignSystemProvider } from "./provider";
+export { default as DesignSystemProvider } from './provider';
 
-export { default as Button } from "./components/Button";
+export { default as Button } from './components/Button';
+export { default as Toggle } from './components/Toggle';
