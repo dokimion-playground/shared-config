eda5561acd5d2b325fd8d7e82514da4c6c4515f9
ss-davidlee <davidlee@styleseller.co.kr>
2024-07-24 17:57:10 +0900

    feat: prettier config

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true, //  true인 경우 {foo:bar}는 { foo: bar }로 변환됨
+  arrowParens: "always",
+  useTabs: false,
+};
