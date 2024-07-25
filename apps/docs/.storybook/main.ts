99ed0decd7e3852282011339370422051436b291
ss-davidlee <davidlee@styleseller.co.kr>
2024-07-25 18:14:23 +0900

    feat: main.js -> main.ts

---
+++ apps/docs/.storybook/main.ts
@@ -0,0 +1,45 @@
+import { dirname, join, resolve } from 'path';
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
+
+const config = {
+  stories: ['../stories/*.stories.tsx', '../stories/**/*.stories.tsx'],
+  addons: [
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-themes'),
+  ],
+  framework: {
+    name: getAbsolutePath('@storybook/react-vite'),
+    options: {},
+  },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+  },
+
+  core: {},
+
+  async viteFinal(config, { configType }) {
+    // customize the Vite config here
+    return {
+      ...config,
+      define: { 'process.env': {} },
+      resolve: {
+        alias: [
+          {
+            find: '@repo/ui',
+            replacement: resolve(__dirname, '../../../packages/ui/'),
+          },
+        ],
+      },
+    };
+  },
+
+  docs: {
+    autodocs: 'tag',
+  },
+};
+
+export default config;
