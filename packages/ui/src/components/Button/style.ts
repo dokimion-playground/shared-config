cea95ca7072a8cb922332598c1c0257c3c959156
ss-davidlee <davidlee@styleseller.co.kr>
2024-07-30 17:50:04 +0900

    feat: modify button style

--- packages/ui/src/components/Button/style.ts
+++ packages/ui/src/components/Button/style.ts
@@ -5,40 +5,72 @@ import { addToPx, FONT_STYLES } from '../../styles/common.style';
 interface ButtonStyleProps {
   $width: string | number;
   $size: ButtonSizes;
-  $variant?: ButtonVariants;
-  disabled?: boolean;
+  $variant: ButtonVariants;
+  disabled: boolean;
   Styles?: CSSProp;
 }
 
-export const buttonSize = {
+const buttonSizeStyles = {
   sm: () => css`
-    padding: 8px 14px;
-    ${FONT_STYLES.BODY3_S}
+    ${FONT_STYLES.BODY6_M}
+    padding: 8px 16px;
   `,
   md: () => css`
-    padding: 10px 16px;
-    ${FONT_STYLES.BODY1_S2}
+    ${FONT_STYLES.BODY3_M2}
+    padding: 14px 16px;
   `,
   lg: () => css`
-    padding: 14px 20px;
     ${FONT_STYLES.H7_S}
+    padding: 20px 16px;
   `,
 };
 
-const disabledButtonStyles = css<ButtonStyleProps>`
+const buttonVariantStyles = {
+  primary: () => css`
+    background-color: ${({ theme }) => theme.color.black};
+    color: ${({ theme }) => theme.color.white};
+  `,
+  secondary: () => css`
+    background-color: ${({ theme }) => theme.color.white};
+    border: 2px solid ${({ theme }) => theme.color.black};
+  `,
+  sub: () => css`
+    background-color: ${({ theme }) => theme.color.white};
+    border: 2px solid ${({ theme }) => theme.color.gray400};
+    &:hover {
+      border: 2px solid ${({ theme }) => theme.color.black};
+    }
+  `,
+  gray: () => css`
+    background-color: ${({ theme }) => theme.color.gray300};
+    &:active {
+      background-color: ${({ theme }) => theme.color.gray400};
+    }
+  `,
+  text: () => css`
+    background-color: inherit;
+    border: none;
+  `,
+};
+
+const disabledStyles = ($size: ButtonStyleProps['$size']) => css`
   cursor: not-allowed;
-  background-color: ${({ theme }) => theme.color['gray100']};
-  color: ${({ theme }) => theme.color['gray500']};
+  background-color: ${({ theme }) => theme.color.gray300};
+  color: ${({ theme }) => theme.color.gray500};
+
+  ${$size === 'lg' &&
+  css`
+    background-color: ${({ theme }) => theme.color.gray400};
+  `};
 `;
 
-export const ButtonWrapper = styled.button<ButtonStyleProps>`
+export const Button = styled.button<ButtonStyleProps>`
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 8px;
-  ${({ $size }) => buttonSize[$size]};
   width: ${({ $width }) => addToPx($width)};
-  ${({ disabled }) => disabled && disabledButtonStyles};
-  border: ${({ theme, $variant }) =>
-    $variant === 'primary' ? `2px solid ${theme.color['black']}` : 'none'};
+  ${({ $size }) => buttonSizeStyles[$size]};
+  ${({ $variant }) => buttonVariantStyles[$variant]};
+  ${({ $size, disabled }) => disabled && disabledStyles($size)};
 `;
