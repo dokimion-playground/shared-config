3c9455bb5bcfaa5ec8db0295b19ca0a36b02fb14
ss-davidlee <davidlee@styleseller.co.kr>
2024-07-31 18:21:58 +0900

    feat: ToastProvider 제거

--- packages/ui/src/provider.tsx
+++ packages/ui/src/provider.tsx
@@ -4,16 +4,11 @@ import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from './styles/global';
 import theme from './styles/theme';
-import { ToastProvider } from './components';
 
 const DesignSystemProvider = ({ children }: PropsWithChildren) => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <ToastProvider>
-        {children}
-        <div id="root-modal"></div>
-      </ToastProvider>
       {children}
     </ThemeProvider>
   );
