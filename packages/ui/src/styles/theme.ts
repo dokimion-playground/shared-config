45fed8f1b742513320f46febad670720752495a4
ss-davidlee <davidlee@styleseller.co.kr>
2024-08-02 09:05:36 +0900

    feat: theme 수정

--- packages/ui/src/styles/theme.ts
+++ packages/ui/src/styles/theme.ts
@@ -1,58 +1,47 @@
 export type Colors = typeof color;
 
 const color = {
-  primary: "#A37BFF",
-  secondary: "#EEE7FF",
-  backgroundLightGreenColor: "#ECFCF9",
+  error: '#FF4842',
+  alarm: '#F73838',
 
-  error: "#FF4842",
-  alarm: "#F73838",
+  point1: '#001BEA',
+  point2: '#015EFB',
+  blue: '#015EFB',
 
-  point1: "#001BEA",
-  point2: "#015EFB",
-  blue: "#015EFB",
+  bg_white: '#343A40',
+  bg_bg1: '#F1F1F3',
+  bg_bg2: '#EBF0F8',
 
-  bg_white: "#343A40",
-  bg_bg1: "#F1F1F3",
-  bg_bg2: "#EBF0F8",
-
-  lineB_B_01: "#D6D6DC",
-  lineB_B_02: "#CCC",
-
-  white: "#FFF",
-  gray50: "#FAFAFA",
-  gray100: "#F8F9FA",
-  gray150: "#F1F1F3",
-  gray200: "#F1F3F5",
-  gray300: "#E9ECEF",
-  gray400: "#CED4DA",
-  gray450: "#D6D6DC",
-  gray500: "#ADB5BD",
-  gray600: "#868E96",
-  gray650: "#616C76",
-  gray700: "#495057",
-  gray800: "#343A40",
-  gray900: "#212529",
-  gray1000: "#191C1F",
-  black: "#000",
-
-  avatar_bg: "#F2F4F7",
-  avatar_font: "#667085",
-
-  gray_background: "#F9FAFB",
+  white: '#FFF',
+  gray50: '#FAFAFA',
+  gray100: '#F8F9FA',
+  gray150: '#F1F1F3',
+  gray200: '#F1F3F5',
+  gray300: '#E9ECEF',
+  gray400: '#CED4DA',
+  gray450: '#D6D6DC',
+  gray500: '#ADB5BD',
+  gray600: '#868E96',
+  gray650: '#616C76',
+  gray700: '#495057',
+  gray800: '#343A40',
+  gray900: '#212529',
+  gray1000: '#191C1F',
+  black: '#000',
 } as const;
 
 export const shadowStyles = {
-  shadow1: "0 2px 4px rgba(0, 0, 0, 0.16)",
-  shadow2: "0 2px 16px rgba(0, 0, 0, 0.1)",
-  shadow3: "0 2px 4px rgba(0, 0, 0, 0.2)",
+  shadow1: '0 2px 4px rgba(0, 0, 0, 0.16)',
+  shadow2: '0 2px 16px rgba(0, 0, 0, 0.1)',
+  shadow3: '0 2px 4px rgba(0, 0, 0, 0.2)',
 } as const;
 
 const zIndex = {
   dropdown: 5,
   header: 10,
   modal: 15,
-  backdrop: 20,
+  backdrop: 30,
+  toast: 20,
 } as const;
 
 const theme = {
